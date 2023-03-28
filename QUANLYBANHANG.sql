737bfa68a02c3c8001fcecd867bf71026166c0e0
huyenhuyen-123 <128460372+huyenhuyen-123@users.noreply.github.com>
2023-03-28 20:57:59 +0700

    Add files via upload

--- QUANLYBANHANG.sql
+++ QUANLYBANHANG.sql
@@ -108,4 +108,83 @@ INSERT INTO Xuat VALUES
 ('X03', 'SP02', 'NV01', '12-12-2020', 1),
 ('X04', 'SP03', 'NV02', '06-02-2020', 2),
 ('X05', 'SP05', 'NV01', '05-18-2020', 1);
+--1.Hiển thị thông tin các bảng dữ liệu trên
+select*from Hangsx;
+select*from Nhanvien;
+select*from Sanpham;
+select*from  Nhap;
+select*from Xuat;
+--2.Đưa ra thông tin masp, tensp, tenhang,soluong, mausac, giaban, donvitinh, mota của các sản phẩm theo chiều giảm dần
+select*from Sanpham order by giaban DESC ;
+--3.Đưa ra các sản phẩm có trong cửa hàng do cty SamSung sản xuất
+select Tensp from Sanpham,Hangsx
+where Sanpham.mahangsx = Hangsx.Mahangsx
+AND tenhang='Samsung'
+--4.Đưa ra thông tin các nhân viên nữ ở phòng kế toán
+select*from Nhanvien where (Gioitinh='Nữ') AND (Phong='Kế toán');
+--5. Đưa ra thông tin phiếu nhập gồm: sohdn, masp, tensp, tenhang, soluongN, dongiaN, tiennhap=soluongN*dongiaN, mausac, donvitinh, ngaynhap, tennv, phong. Sắp xếp theo chiều tăng dần của hóa đơn nhập.
+SELECT Nhap.Sohdn, Sanpham.Masp, Sanpham.Tensp, Hangsx.Tenhang, Nhap.SoluongN, Nhap.DongiaN, Nhap.SoluongN*Nhap.DongiaN AS tiennhap, Sanpham.Mausac, Sanpham.Donvitinh, Nhap.Ngaynhap, Nhanvien.Tennv, Nhanvien.Phong
+FROM Nhap
+JOIN Sanpham ON Nhap.Masp = Sanpham.Masp
+JOIN Hangsx ON Sanpham.Mahangsx= Hangsx.Mahangsx
+JOIN Nhanvien ON Nhap.Manv = Nhanvien.Manv
+ORDER BY Nhap.Sohdn ASC;
+--6. Đưa ra thông tin phiếu xuất gồm: sohdx, masp, tensp, tenhang, soluongX, giaban, tienxuatsoluongX*giaban, mausac, donvitinh, ngayxuat, tennv, phong trong tháng 10 năm 2018, sắp xếp theo chiều tăng dần của sohdx.
+SELECT Xuat.SoHDX, Xuat.MaSP, SanPham.TenSP, HangSX.Tenhang, Xuat.SoLuongX,SanPham.GiaBan,Xuat.SoLuongX*SanPham.GiaBan AS TienXuat, SanPham.MauSac, SanPham.DonViTinh, Xuat.NgayXuat, NhanVien.TenNV, NhanVien.Phong
+FROM Xuat
+INNER JOIN SanPham ON Xuat.MaSP = SanPham.MaSP
+INNER JOIN HangSX ON SanPham.MaHangSX = HangSX.MaHangSX
+INNER JOIN NhanVien ON Xuat.MaNV = NhanVien.MaNV
+ORDER BY Xuat.SoHDX ASC
+--7. Đưa ra các thông tin về các hóa đơn mà hãng samsung đã nhập trong năm 2017, gồm: sohdn, masp, tensp, soluongN, dongiaN, ngaynhap, tennv, phong.
+select sohdn, Sanpham.masp, tensp, soluongN, dongiaN, ngaynhap, tennv, phong,tenhang
+from nhap, sanpham, nhanvien, Hangsx
+where year(ngaynhap)= 2017 and Hangsx.tenhang='Samsung'
+--8. Đưa ra Top 10 hóa đơn xuất có số lượng xuất nhiều nhất trong năm 2018, sắp xếp theo chiều giảm dần của soluongX.
 
+--9. Đưa ra thông tin 10 sản phẩm có giá bán cao nhất trong cửa hàng, theo chiều giảm dần giá bán.
+SELECT TOP(10) tenSP, giaBan
+FROM SanPham
+ORDER BY giaBan DESC;
+--10. Đưa ra các thông tin sản phẩm có gía bán từ 100.000 đến 500.000 của hãng samsung.
+SELECT * FROM Sanpham
+JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+WHERE Hangsx.tenhang = 'Samsung' AND Sanpham.giaban >= 100000 AND Sanpham.giaban <= 500000
+--11. Tính tổng tiền đã nhập trong năm 2018 của hãng samsung.
+SELECT SUM(SoluongN * DongiaN) AS TongTien
+FROM Nhap
+JOIN Sanpham ON Nhap.Masp=Sanpham.Masp
+JOIN Hangsx ON Sanpham.Mahangsx=Hangsx.Mahangsx
+WHERE Hangsx.Tenhang= 'Samsung' AND YEAR(Ngaynhap) = 2018 
+--12. Thống kê tổng tiền đã xuất trong ngày 2/9/2018
+SELECT SUM(Xuat.SoluongX * Sanpham.Giaban) AS TongTien
+FROM Xuat
+INNER JOIN Sanpham ON Xuat.Masp = Sanpham.Masp
+WHERE Xuat.Ngayxuat = '2018-09-02'
+--13. Đưa ra sohdn, ngaynhap có tiền nhập phải trả cao nhất trong năm 2018
+SELECT TOP 1 Sohdn, Ngaynhap, DongiaN
+FROM Nhap
+ORDER BY DongiaN DESC
+--14. Đưa ra 10 mặt hàng có soluongN nhiều nhất trong năm 2019.
+SELECT TOP 10 Sanpham.Tensp, SUM(Nhap.SoluongN) AS TongSoLuongN 
+FROM Sanpham 
+INNER JOIN Nhap ON Sanpham.Masp = Nhap.Masp 
+WHERE YEAR(Nhap.Ngaynhap) = 2019 
+GROUP BY Sanpham.Tensp 
+ORDER BY TongSoLuongN DESC
+--15. Đưa ra masp,tensp của các sản phẩm do công ty ‘Samsung’ sản xuất do nhân viên có mã ‘NV01’ nhập.
+SELECT Sanpham.Masp, Sanpham.Tensp
+FROM Sanpham
+INNER JOIN Hangsx ON Sanpham.Mahangsx = Hangsx.Mahangsx
+INNER JOIN Nhap ON Sanpham.Masp = Nhap.Masp
+INNER JOIN Nhanvien ON Nhap.Manv = Nhanvien.Manv
+WHERE Hangsx.Tenhang = 'Samsung' AND Nhanvien.Manv = 'NV01';
+--16. Đưa ra sohdn,masp,soluongN,ngayN của mặt hàng có masp là ‘SP02’, được nhân viên ‘NV02′ xuất.
+SELECT Sohdn, Masp, SoluongN, Ngaynhap
+FROM Nhap
+WHERE Masp = 'SP02' AND Manv = 'NV02'
+--17. Đưa ra manv,tennv đã xuất mặt hàng có mã ‘SPO2′ ngày 03-02-2020.
+SELECT Nhanvien.manv, Nhanvien.tennv
+FROM Nhanvien
+JOIN Xuat ON Nhanvien.Manv = Xuat.Manv
+WHERE Xuat.Masp = 'SP02' AND Xuat.Ngayxuat = '2020-03-02'
